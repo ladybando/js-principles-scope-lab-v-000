134df5603f0f66d171dcc7045361d70e3161e177
Ayana Bando <ayanaflax@yahoo.com>
2018-11-24 20:07:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var customerName = 'bob';
 const leastFavoriteCustomer = "not bob";
 
 function upperCaseCustomerName(){
-  var customerName.toUpperCase();
+  customerName.toUpperCase();
 }
 
 function setBestCustomer(){
